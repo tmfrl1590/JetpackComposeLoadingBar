81b3c2b2cd1e09926cadcb7aa37302544af47796
seulgi <tmfrl1590@gmail.com>
2025-02-24 22:31:01 +0900

    Chore: targetSdkVersion to 35 and update dependencies
    - targetSdk, compileSdk 34 -> 35
    - Added android.suppressUnsupportedCompileSdk=35
    - Restored ic_launcher_background.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.tmfrl.compose.loading"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.tmfrl.compose.loading"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -30,8 +30,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
         jvmTarget = "1.8"
