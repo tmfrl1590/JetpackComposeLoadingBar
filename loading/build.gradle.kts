1396b371865824daafef18f72e8ab2dd286bbd5f
seulgi <tmfrl1590@gmail.com>
2025-06-11 11:21:09 +0900

    chore: support Android 8 by lowering minSdk to 26

--- loading/build.gradle.kts
+++ loading/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        minSdk = 28
+        minSdk = 26
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
